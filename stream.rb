0c7479978225d9c526542fa627f32b0302b9da0b
Nat Welch <nat@natwelch.com>
2014-08-08 06:41:20 -0700

    auth works

--- stream.rb
+++ stream.rb
@@ -12,7 +12,8 @@ YOUTUBE_API_VERSION = "v3"
 
 def build_client
   google_api_scopes = [YOUTUBE_READ_WRITE_SCOPE]
-  google_secrets = Google::APIClient::ClientSecrets.load(File.expand_path("~/Dropbox/ThePortholeAgency.json"))
+  google_secrets = Oj.load(File.read(File.expand_path("~/Dropbox/ThePortholeAgency.json")))
+  google_secrets["p12_key"] = File.expand_path("~/Dropbox/ThePortholeAgency.p12")
 
   api_client_options = {
     :application_name => "porthole",
@@ -20,12 +21,16 @@ def build_client
   }
 
   client = Google::APIClient.new(api_client_options)
-  flow = Google::APIClient::InstalledAppFlow.new(
-    :client_id => client_secrets.client_id,
-    :client_secret => client_secrets.client_secret,
-    :scope => google_api_scopes
-  )
-  client.authorization = flow.authorize
+  client.authorization = Signet::OAuth2::Client.new({
+    :audience => 'https://accounts.google.com/o/oauth2/token',
+    :auth_provider_x509_cert_url => 'https://www.googleapis.com/oauth2/v1/certs',
+    :client_x509_cert_url => "https://www.googleapis.com/robot/v1/metadata/x509/#{google_secrets["client_email"]}",
+    :issuer => google_secrets["client_email"],
+    :scope => google_api_scopes,
+    :signing_key => ::Google::APIClient::KeyUtils.load_from_pkcs12(google_secrets["p12_key"], 'notasecret'),
+    :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
+  })
+  client.authorization.fetch_access_token!
   youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
 
   return [client, youtube]
@@ -46,10 +51,13 @@ end
 
 def main
   client, youtube = build_client
-  p youtube
+  puts "Valid youtube requests: #{youtube.singleton_methods.inspect}"
+  params = {
+    :part => ""
+  }
 
-  puts "======== INSERT"
-  p request(client, youtube.livebroadcast.insert)
+  puts "======== INSERT ATTEMPT"
+  p request(client, youtube.live_broadcasts.insert, params)
 end
 
 main
