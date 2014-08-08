c085316b649661b70cb488261226a5e4deaad8b1
Nat Welch <nat@natwelch.com>
2014-08-08 06:17:55 -0700

    Attempt to send an empty request

--- stream.rb
+++ stream.rb
@@ -1,8 +1,17 @@
-require 'bundler/setup'
+#! /usr/bin/env ruby
 
+require 'bundler/setup'
+Bundler.require(:default)
+
+require 'google/api_client'
+require 'google/api_client/client_secrets'
+
+YOUTUBE_READ_WRITE_SCOPE = "https://www.googleapis.com/auth/youtube"
+YOUTUBE_API_SERVICE_NAME = "youtube"
+YOUTUBE_API_VERSION = "v3"
 
 def build_client
-  google_api_scope_url = "https://www.googleapis.com/auth/youtube"
+  google_api_scopes = [YOUTUBE_READ_WRITE_SCOPE]
   google_secrets = Google::APIClient::ClientSecrets.load(File.expand_path("~/Dropbox/ThePortholeAgency.json"))
 
   api_client_options = {
@@ -10,22 +19,19 @@ def build_client
     :application_version => "0.0.1"
   }
 
-  client = ::Google::APIClient.new(api_client_options)
-  client.authorization = Signet::OAuth2::Client.new({
-    :audience => 'https://accounts.google.com/o/oauth2/token',
-    :auth_provider_x509_cert_url => 'https://www.googleapis.com/oauth2/v1/certs',
-    :client_x509_cert_url => "https://www.googleapis.com/robot/v1/metadata/x509/#{google_client_email}",
-    :issuer => google_client_email,
-    :scope => google_api_scope_url,
-    :signing_key => ::Google::APIClient::KeyUtils.load_from_pkcs12(google_key, 'notasecret'),
-    :token_credential_uri => 'https://accounts.google.com/o/oauth2/token',
-  })
-  client.authorization.fetch_access_token!
+  client = Google::APIClient.new(api_client_options)
+  flow = Google::APIClient::InstalledAppFlow.new(
+    :client_id => client_secrets.client_id,
+    :client_secret => client_secrets.client_secret,
+    :scope => google_api_scopes
+  )
+  client.authorization = flow.authorize
+  youtube = client.discovered_api(YOUTUBE_API_SERVICE_NAME, YOUTUBE_API_VERSION)
 
-  return client
+  return [client, youtube]
 end
 
-def request(client, api_method, parameters, body_object = nil)
+def request(client, api_method, parameters = {}, body_object = nil)
   client_parms = {
     :api_method => api_method,
     :parameters => parameters,
@@ -37,3 +43,13 @@ def request(client, api_method, parameters, body_object = nil)
   # Returns an array of body and status code
   return [result.body.nil? || result.body.empty? ? nil : Oj.load(result.body), result.status]
 end
+
+def main
+  client, youtube = build_client
+  p youtube
+
+  puts "======== INSERT"
+  p request(client, youtube.livebroadcast.insert)
+end
+
+main
